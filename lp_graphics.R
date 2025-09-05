80a872e185d1b7a02b21a1aa586bc140950e7926
Evan Krause <evankrause@protonmail.com>
2025-09-05 14:56:22 -0400

    create lp_report quarto doc/ edits to lp_clean

--- lp_graphics.R
+++ lp_graphics.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(gtsummary)
 library(GGally)
+library(gt)
 
 source('src/lp_clean.R')
 
@@ -27,6 +28,18 @@ lp_dat |>
   add_overall()
   
 
+
+lp_dat |>
+  select(3:length(lp_dat)) |>
+  tbl_summary(by = station,
+              statistic = list(all_continuous() ~ "{median} ({p25} - {p75})",
+                               all_categorical() ~ "{n} {p}")) |>
+  bold_labels() |>
+  add_overall()
+lp_dat |>
+  select(3:length(lp_dat)) |>
+  gt(groupname_col = lp_dat$station)
+
 # graphics + plots ----
 
 ## pairplots ----
@@ -39,11 +52,19 @@ lp_dat |>
 
 ## point-plots ----
 
+
+#column profile?
 lp_dat |>
-  ggplot() +
+  ggplot(aes(color = c(Depth_m))) +
   geom_point(aes(Depth_m, pH)) +
   geom_line(aes(Depth_m, pH)) +
   # geom_point(aes(Depth_m, `Dissolved Oxygen`)) +
-  geom_point(aes(Depth_m, I(Chlorophyll), colour = 1)) +
-  facet_grid(~date) +
-  coord_flip()
+  geom_point(aes(Depth_m, Chlorophyll)) +
+  geom_line(aes(Depth_m, Chlorophyll)) + 
+  geom_point(aes(Depth_m, `Dissolved Oxygen`)) +
+  geom_line(aes(Depth_m, `Dissolved Oxygen`)) +
+  geom_point(aes(Depth_m, pH)) +
+  geom_line(aes(Depth_m, pH)) +
+  facet_wrap(~date, scales = 'free_x') +
+  coord_flip() +
+  scale_x_reverse()
